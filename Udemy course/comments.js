aab9cf7366685341c4b3bd52e626b0b902210444
Geovanna Martins <geovannacabral2018@gmail.com>
2022-01-10 18:14:35 -0300

    comments

--- Udemy course/comments.js
+++ Udemy course/comments.js
@@ -1,1 +1,13 @@
+//There is the first time that i used comments 
+console.log('hello') //comments can stay next to codes 
+/*
+comments 
+can 
+have
+more 
+than 
+one 
+line 
+*/
+//this type of comment  with "//" come from the c language 
 
