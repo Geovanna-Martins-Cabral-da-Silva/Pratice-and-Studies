dea972229a580eb26c90f5e48df7f188ac026b93
Geovanna Martins <geovannacabral2018@gmail.com>
2022-01-10 17:54:21 -0300

    new javascript codes

---
+++ Udemy course/code sentence.js
@@ -0,0 +1,22 @@
+console.log(1 + 3);
+console.log(7 / 8);
+console.log( 8** 2);
+console.log(8 < 7)
+console.log(8 != 7)
+console.log(99-10+1+80)
+console.log (7 % 3)
+console.log( 20 % 5)
+20/5 
+const divisao =Math.floor(20/5)
+console.log(divisao) //1
+
+var divi =Math.floor(20/5)
+console.log(divi)
+
+let div =Math.floor(20/5)
+console.log(div)
+
+
+
+
+
