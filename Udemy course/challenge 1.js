97bd425ad1b4f05d357daa0e23f1b3418ab22f4d
Geovanna Martins <geovannacabral2018@gmail.com>
2022-01-12 09:41:14 -0300

    more codes

---
+++ Udemy course/challenge 1.js
@@ -0,0 +1,16 @@
+/* code two code blocks, one that show the even numbers and another that show the old number*/
+{
+    console.log('1')
+    console.log('3')
+    console.log('5')
+    console.log('7')
+    console.log('9')
+}
+
+{
+    console.log('2')
+    console.log('4')
+    console.log('6')
+    console.log('8')
+    console.log('10')
+}
